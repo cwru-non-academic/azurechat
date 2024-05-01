b12c73374c0f0daafd1ff9481d70217e18a003ef
sean murphy <smm102@case.edu>
2024-05-01 15:21:05 -0400

    Chaged Azure Chat Name and disabled git sign in

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -31,8 +31,8 @@ export const LogIn: FC<LoginProps> = (props) => {
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
+        {/*<Button onClick={() => signIn("github")}>GitHub</Button>*/}
+        <Button onClick={() => signIn("azure-ad")}>CWRU Single Sign On</Button>
         {props.isDevMode ? (
           <Button onClick={() => signIn("localdev")}>
             Basic Auth (DEV ONLY)
